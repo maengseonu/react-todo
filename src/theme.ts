c16e9c3084bd619b2462eeed3f48353aefb1fdbe
maengseonu <aodtjsdn123@gmail.com>
2021-12-02 15:19:40 +0900

    first commit

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "#8c7ae6",
+  borderColor: "white",
+};
