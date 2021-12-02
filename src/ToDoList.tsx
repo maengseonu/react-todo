43e1d500141fe2ad8d331f9e28e433ea730a5d39
maengseonu <aodtjsdn123@gmail.com>
2021-12-02 17:33:04 +0900

    react-hook-form(Validation)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -30,20 +30,104 @@ import { useForm } from "react-hook-form";
   );
 } */
 
+interface IForm {
+  Email: string;
+  FirstName: string;
+  LastName: string;
+  UserName: string;
+  Password: string;
+  Password1: string;
+  extraError?: string;
+}
+
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      Email: "@naver.com",
+    },
+  });
+
+  const onValid = (data: IForm) => {
+    if (data.Password !== data.Password1) {
+      setError(
+        "Password1",
+        { message: "비밀번호가 같지 않습니다." },
+        { shouldFocus: true }
+      );
+    }
+    // setError("extraError", { message: "Server offline." });
+  };
+  console.log(errors);
 
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("FirstName")} placeholder="First Name" />
-        <input {...register("LastName")} placeholder="Last Name" />
-        <input {...register("UserName")} placeholder="User Name" />
-        <input {...register("Password ")} placeholder="Password " />
-        <input {...register("Password1 ")} placeholder="Password1 " />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("Email", {
+            required: "필수항목입니다",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.Email?.message}</span>
+        <input
+          {...register("FirstName", {
+            required: "필수항목입니다",
+            validate: {
+              noSeonu: (value) =>
+                value.includes("선우") ? "선우는 사용 할 수없습니다" : true,
+              noSuhyun: (value) =>
+                value.includes("수현") ? "수현은 사용 할 수없습니다" : true,
+            },
+          })}
+          placeholder="First Name"
+        />
+        <span>{errors?.FirstName?.message}</span>
+        <input
+          {...register("LastName", { required: "필수항목입니다" })}
+          placeholder="Last Name"
+        />
+        <span>{errors?.LastName?.message}</span>
+        <input
+          {...register("UserName", {
+            required: "필수항목입니다",
+            minLength: 10,
+          })}
+          placeholder="User Name"
+        />
+        <span>{errors?.UserName?.message}</span>
+        <input
+          {...register("Password", {
+            required: "필수항목입니다",
+            minLength: 5,
+          })}
+          placeholder="Password"
+        />
+        <span>{errors?.Password?.message}</span>
+        <input
+          {...register("Password1", {
+            required: "필수항목입니다",
+            minLength: {
+              value: 5,
+              message: "패스워드가 짧습니다.",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.Password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
